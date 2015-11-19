20cc05523c09297163a164ff30be255be6dcd1d8
Eric Huppert <ehuppert@mit.edu>
2015-11-19 02:35:22 +0000

    tiny optimizations

--- player/move_gen.h
+++ player/move_gen.h
@@ -40,7 +40,7 @@ typedef int fil_t;
 
 #define PIECE_SIZE 5  // Number of bits in (ptype, color, orientation)
 
-typedef int piece_t;
+typedef int8_t piece_t;
 
 // -----------------------------------------------------------------------------
 // piece types
@@ -174,7 +174,7 @@ void move_to_str(move_t mv, char *buf, size_t bufsize);
 int generate_all(position_t *p, sortable_move_t *sortable_move_list,
                  bool strict);
 void do_perft(position_t *gme, int depth, int ply);
-piece_t low_level_make_move(position_t *old, position_t *p, move_t mv);
+square_t low_level_make_move(position_t *old, position_t *p, move_t mv);
 victims_t make_move(position_t *old, position_t *p, move_t mv);
 void display(position_t *p);
 uint64_t compute_zob_key(position_t *p);
