2b09c062569a31b4b940c4995e95134a06bec85d
Eric Huppert <ehuppert@mit.edu>
2015-11-21 20:58:30 +0000

    pcentral lookup table, not much faster

--- player/leiserchess.c
+++ player/leiserchess.c
@@ -398,7 +398,6 @@ void print_options() {
 int main(int argc, char *argv[]) {
   position_t *gme = (position_t *) malloc(sizeof(position_t) * MAX_PLY_IN_GAME);
 
-  init_eval();
 
   setbuf(stdout, NULL);
   setbuf(stdin, NULL);
@@ -411,6 +410,7 @@ int main(int argc, char *argv[]) {
   }
 
   init_options();
+  init_eval();
   init_zob();
 
   char **tok = (char **) malloc(sizeof(char *) * MAX_CHARS_IN_TOKEN * MAX_PLY_IN_GAME);
