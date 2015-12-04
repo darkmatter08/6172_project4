8f6b5cc86e830ead1d972bf4d98049254069d93d
Berj Chilingirian <berjc@mit.edu>
2015-12-04 02:54:33 +0000

    make locks sleep and remove stack allocation

--- player/simple_mutex.h
+++ player/simple_mutex.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2015 MIT License by 6.172 Staff
-
+#include <xmmintrin.h>
 // A simple implementation of a spin lock using compare and swap.
 #ifndef SIMPLE_MUTEX_H
 #define SIMPLE_MUTEX_H
@@ -13,14 +13,17 @@ void init_simple_mutex(simple_mutex_t* mutex) {
 
 void simple_acquire(simple_mutex_t* mutex) {
   while (!__sync_bool_compare_and_swap(mutex, 0, 1)) {
-    continue;  // Did not acquire lock yet.
+    //_mm_pause();
+    usleep(1);
+    //continue;  // Did not acquire lock yet.
   }
 }
 
 void simple_release(simple_mutex_t* mutex) {
-  if (!__sync_bool_compare_and_swap(mutex, 1, 0)) {
+  /*if (!__sync_bool_compare_and_swap(mutex, 1, 0)) {
     printf("ERROR!\n");
-  }
+  }*/
+  *mutex = 0;
 }
 
 #endif  // SIMPLE_MUTEX_H
