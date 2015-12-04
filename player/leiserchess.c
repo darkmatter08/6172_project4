39dd5d0d0eae07ccdfebd240c279580978e47344
Berj Chilingirian <berjc@mit.edu>
2015-12-03 19:39:28 -0500

    change test script to test serial code

--- player/leiserchess.c
+++ player/leiserchess.c
@@ -12,11 +12,15 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 
-//#if PARALLEL
 #include <cilk/cilk.h>
 #include <cilk/reducer.h>
 #include <cilk/cilk_api.h>
-//#endif
+
+#if TEST
+  #define NUM_CILK_WRKRS "1"
+#else
+  #define NUM_CILK_WRKRS "8"
+#endif
 
 #include "./eval.h"
 #include "./fen.h"
@@ -399,7 +403,7 @@ void print_options() {
 // -----------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
-  __cilkrts_set_param("nworkers", "1");
+  __cilkrts_set_param("nworkers", NUM_CILK_WRKRS);
   position_t *gme = (position_t *) malloc(sizeof(position_t) * MAX_PLY_IN_GAME);
 
 
